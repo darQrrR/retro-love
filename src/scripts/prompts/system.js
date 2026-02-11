6794812854a97d7a0b8960b046b1d068052bbfc1
Marc Lang <marc.lang@visions.ch>
2026-02-11 22:27:43 +0100

    feat: rem basic functionality

--- src/scripts/prompts/system.js
+++ src/scripts/prompts/system.js
@@ -2,17 +2,21 @@ import { promptTypes } from '../app/constants.js';
 import { dom } from '../ui/dom.js';
 
 export async function CLR() {
-	dom.clearOutput();
-	dom.clearInput();
+  dom.clearOutput();
+  dom.clearInput();
 
-	return { type: promptTypes.DEFAULT, value: null };
+  return { type: promptTypes.DEFAULT, value: null };
+}
+
+export async function REM(message) {
+  return { type: promptTypes.DEFAULT, value: null };
 }
 
 export async function EXIT() {
-	return;
+  return { type: promptTypes.DEFAULT, value: null };
 }
 
 // debug log
 export const C64LOG = message => {
-	dom.outputLine(message);
+  dom.outputLine(message);
 };
