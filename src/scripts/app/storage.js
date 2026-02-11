b105f8402aad9a84723eba0c88a92d0b7a09597b
Marc Lang <marc.lang@visions.ch>
2026-02-11 22:26:57 +0100

    feat: storage cleaner

--- src/scripts/app/storage.js
+++ src/scripts/app/storage.js
@@ -2,21 +2,32 @@ export let promptStorage = [];
 export let variableStorage = [];
 
 // store prompt
-export const storePrompt = prompt => {
-	// if linenumer already exists -> remove
-	const exists = promptStorage.findIndex(
-		obj => obj.lineNumber === prompt.lineNumber,
-	);
-	if (exists !== -1) promptStorage.splice(exists, 1);
+export const storePrompt = (prompt) => {
+  // if linenumer already exists -> remove
+  const exists = promptStorage.findIndex(
+    (obj) => obj.lineNumber === prompt.lineNumber,
+  );
+  if (exists !== -1) promptStorage.splice(exists, 1);
 
-	// push and sort new prompt
-	promptStorage.push(prompt);
-	promptStorage.sort((a, b) => a.lineNumber - b.lineNumber);
-	//console.log(promptStorage);
+  // push and sort new prompt
+  promptStorage.push(prompt);
+  promptStorage.sort((a, b) => a.lineNumber - b.lineNumber);
 };
 
 // store variable
 export const storeVariable = (name, value) => {
-	variableStorage[name] = value;
-	//console.log(variableStorage);
+  variableStorage[name] = value;
+};
+
+export const clearPromptStorage = () => {
+  promptStorage = [];
+};
+
+export const clearVariableStorage = () => {
+  variableStorage = [];
+};
+
+export const clearAllStorage = () => {
+  promptStorage = [];
+  variableStorage = [];
 };
